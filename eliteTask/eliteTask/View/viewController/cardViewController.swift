ddf4867443e471197d21a9899e3edf2bd73c6707
RishanRashid <rishanrashi@gmail.com>
2024-07-22 12:01:43 +0530

    CollectionScrollingAdjusted

--- eliteTask/eliteTask/View/viewController/cardViewController.swift
+++ eliteTask/eliteTask/View/viewController/cardViewController.swift
@@ -12,11 +12,11 @@ class cardViewController: UIViewController, UICollectionViewDelegate, UICollecti
     
     private let viewModel = mainViewModel()
     @IBOutlet weak var collectionView: UICollectionView!
-    var activityIndicator : NVActivityIndicatorView!
+    var activityIndicator: NVActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let frame = CGRect(x: self.view.bounds.width/2 - 25, y: self.view.bounds.height/2 - 25, width: 45, height: 45)
+        let frame = CGRect(x: self.view.bounds.width / 2 - 25, y: self.view.bounds.height / 2 - 25, width: 45, height: 45)
         activityIndicator = NVActivityIndicatorView(frame: frame, type: .ballRotate, color: .blue, padding: nil)
         self.view.addSubview(activityIndicator)
         activityIndicator.isHidden = false
@@ -25,15 +25,15 @@ class cardViewController: UIViewController, UICollectionViewDelegate, UICollecti
         fetchData()
     }
     
-    
-    func collectionSetup(){
+    func collectionSetup() {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(UINib(nibName: "cardCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cardCollectionViewCell")
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        layout.itemSize = CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+        layout.itemSize = CGSize(width: collectionView.frame.width * 0.9, height: collectionView.frame.height)
         layout.minimumLineSpacing = 2
+        layout.sectionInset = UIEdgeInsets(top: 0, left: (collectionView.frame.width - layout.itemSize.width) / 2, bottom: 0, right: (collectionView.frame.width - layout.itemSize.width) / 2)
         collectionView.collectionViewLayout = layout
     }
     
@@ -43,28 +43,26 @@ class cardViewController: UIViewController, UICollectionViewDelegate, UICollecti
         viewModel.fetchProfiles()
     }
     
-    
     func viewModelBinding() {
-            viewModel.onProfilesUpdated = { [weak self] in
-                self?.collectionView.reloadData()
-                self?.stopActivityIndicator()
-            }
-            
-            viewModel.onError = { [weak self] errorMessage in
-                let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                self?.present(alert, animated: true, completion: nil)
-                self?.stopActivityIndicator()
-            }
+        viewModel.onProfilesUpdated = { [weak self] in
+            self?.collectionView.reloadData()
+            self?.stopActivityIndicator()
         }
         
-        func stopActivityIndicator() {
-            DispatchQueue.main.async  { [weak self] in
-                self?.activityIndicator.stopAnimating()
-                self?.activityIndicator.isHidden = true
-            }
+        viewModel.onError = { [weak self] errorMessage in
+            let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self?.present(alert, animated: true, completion: nil)
+            self?.stopActivityIndicator()
         }
+    }
     
+    func stopActivityIndicator() {
+        DispatchQueue.main.async { [weak self] in
+            self?.activityIndicator.stopAnimating()
+            self?.activityIndicator.isHidden = true
+        }
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numberOfProfiles
@@ -75,27 +73,40 @@ class cardViewController: UIViewController, UICollectionViewDelegate, UICollecti
         
         let profile = viewModel.getProfile(at: indexPath.row)
         cell.nameLabel.text = "\(profile.profile_first_name ?? "") \(profile.profile_last_name ?? "")"
-        cell.companyLabel.text = "\(profile.profile_job_title ?? "") at  \(profile.profile_company_name ?? "")"
-        if indexPath.item == 1  || indexPath.item == 3{
+        cell.companyLabel.text = "\(profile.profile_job_title ?? "") at \(profile.profile_company_name ?? "")"
+        if indexPath.item == 1 || indexPath.item == 3 {
             cell.switchControl.isOn = false
         }
         
 //        if let imageUrlString = profile.profile_image_name, let imageUrl = URL(string: imageUrlString) {
-//                    serviceFile.shared.downloadImage(from: imageUrl) { image in
-//                        DispatchQueue.main.async {
-//                            cell.userimage.image = image
-//                        }
-//                    }
-//                } else {
-//                    cell.userimage.image = UIImage(named: "placeholder")
+//            serviceFile.shared.downloadImage(from: imageUrl) { image in
+//                DispatchQueue.main.async {
+//                    cell.userimage.image = image
 //                }
+//            }
+//        } else {
+//            cell.userimage.image = UIImage(named: "placeholder")
+//        }
+        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let collectionViewWidth = collectionView.frame.width
-        let cellWidth = collectionViewWidth * (0.90)
+        let cellWidth = collectionViewWidth * 0.9
         let cellHeight = collectionView.frame.height
         return CGSize(width: cellWidth, height: cellHeight)
     }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        
+        offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
+        targetContentOffset.pointee = offset
     }
+}
